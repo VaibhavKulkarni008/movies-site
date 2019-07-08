1411f9f72db145ca202e234330a3ef55b96a703f
VaibhavKulkarni05 <vaibhav.kulkarni@flyrobe.com>
2019-07-08 23:52:19 +0530

    migration file

--- source/films/models.py
+++ source/films/models.py
@@ -22,7 +22,7 @@ class Genre(models.Model):
 
 class Movie(models.Model):
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     imdb_score = models.DecimalField(max_digits=3,
                                      decimal_places=1,
